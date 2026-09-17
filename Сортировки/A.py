89b79fb71881fc2b506e116e121574dcca6597e9
fbocharot-ux <fbocharot@gmail.com>
2026-09-17 19:43:31 +0300

    Update A.py

--- Сортировки/A.py
+++ Сортировки/A.py
@@ -1,1 +1,11 @@
+def SelectionSort(a):
+    for i in range(len(a)-1):
+        max_index=i
+        for j in range(i+1, len(a)):
+            if a[j]>a[max_index]:
+                max_index=j
+        a[i],a[max_index]=a[max_index],a[i]
 
+a=list(map(int,input().split()))
+SelectionSort(a)
+print(*a)
